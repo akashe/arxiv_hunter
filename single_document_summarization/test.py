e5b6600d78e15ba54a4ef93f15d5a3fc26112c68
akashe <infiniteakashe@gmail.com>
2022-10-03 16:50:26 +0530

    removing old summarization files

--- single_document_summarization/test.py
+++
@@ -1,6 +0,0 @@
-from goose import Goose
-# url = 'http://edition.cnn.com/2012/02/22/world/europe/uk-occupy-london/index.html?hpt=ieu_c2'
-url = 'https://www.cricbuzz.com/cricket-news/117676/nkrumah-bonner-adept-at-the-waiting-game'
-g = Goose()
-article = g.extract(url=url)
-print(article.cleaned_text)
