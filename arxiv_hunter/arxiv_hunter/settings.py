78b84d46ba5236c090d21dfe10a429f45062c7d6
Subrata Mondal <subratasubha2@gmail.com>
2024-01-28 22:05:09 +0530

    add my_auth

--- arxiv_hunter/arxiv_hunter/settings.py
+++ arxiv_hunter/arxiv_hunter/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_bootstrap5',
+    'crispy_forms',
+    'my_auth.apps.MyAuthConfig'
 ]
 
 MIDDLEWARE = [
