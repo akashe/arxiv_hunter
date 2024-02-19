0e52233dee9a25d749313ba51b211063392d194d
Subrata Mondal <subratasubha2@gmail.com>
2024-02-19 12:15:36 +0530

    UI Complete

--- frontend/react-app/src/App.jsx
+++ frontend/react-app/src/App.jsx
@@ -1,14 +1,14 @@
 import './App.css'
 import Header from './components/Header'
 import Main from './components/Main'
-import Footer from './components/Footer'
+import SearchBar from './components/SearchBar'
 
 function App() {
   return (
-    <div className='max-w-[800px] min-w-min bg-white mx-auto'>
+    <div className='max-w-[800px] min-w-min bg-white mx-auto m-0'>
       <Header></Header>
+      <SearchBar></SearchBar>
       <Main></Main>
-      <Footer></Footer>
     </div>
   )
 }
---
+++ frontend/react-app/src/components/SearchBar.jsx
@@ -0,0 +1,14 @@
+function SearchBar ()
+{
+    return <div className="fixed top-16 mt-2 mx-6 left-0 right-0 z-10  h-16 flex items-center justify-center">
+        <div className="relative flex items-center w-full max-w-[800px] min-w-min">
+            <input type="text" className="w-full px-4 py-4 rounded-full bg-slate-300 text-gray-700 focus:outline-none focus:ring-2 focus:ring-slate-800 focus:ring-opacity-60" placeholder="Search here..."/>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 absolute right-4 top-1/2 transform -translate-y-1/2">
+                <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+            </svg>
+
+        </div>
+    </div>
+}
+
+export default SearchBar
