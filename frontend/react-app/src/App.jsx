e3114d50696d8f5000564b4bca4952da261f47d6
Subrata Mondal <subratasubha2@gmail.com>
2024-02-20 11:53:09 +0530

    Implemented API Integration

--- frontend/react-app/src/App.jsx
+++ frontend/react-app/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Header from './components/Header'
 import Main from './components/Main'
 import SearchBar from './components/SearchBar'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
