cbea2dabe4a081732dd89d0d49c25d45a5debcdf
Subrata Mondal <subratasubha2@gmail.com>
2024-02-26 00:09:41 +0530

    Cleared Codes

--- src/app/models.py
+++ src/app/models.py
@@ -24,33 +24,5 @@ class User(_database.BASE):
     id = _sql.Column(_sql.Integer, primary_key=True, index=True)
     email = _sql.Column(_sql.String, unique=True, index=True)
     hashed_password = _sql.Column(_sql.String)
+    preferences = _sql.Column(_sql.String, default="")
     is_active = _sql.Column(_sql.Boolean, default=True)
-
-    # relationship with 'Post' model
-    posts = _orm.relationship("Post", back_populates="owner")
-
-
-class Post(_database.BASE):
-    """Represents a post created by a user.
-
-    Maps to the "posts" table with columns for:
-        - id: Unique identifier (primary key)
-        - title: Title of the post
-        - content: Content of the post
-        - owner_id: Foreign key referencing the user who created the post
-        - date_created: Date and time the post was created
-        - date_last_updated: Date and time the post was last updated
-
-    Belongs to the User model, meaning a post is created by a specific user.
-    """
-
-    __tablename__ = "posts"
-    id = _sql.Column(_sql.Integer, primary_key=True, index=True)
-    title = _sql.Column(_sql.String, index=True)
-    content = _sql.Column(_sql.String, index=True)
-    owner_id = _sql.Column(_sql.Integer, _sql.ForeignKey("users.id"))
-    date_created = _sql.Column(_sql.DateTime, default=_dt.datetime.utcnow)
-    date_last_updated = _sql.Column(_sql.DateTime, default=_dt.datetime.utcnow)
-
-    # relationship with User model
-    owner = _orm.relationship("User", back_populates="posts")
--- src/app/schemas.py
+++ src/app/schemas.py
@@ -7,37 +7,8 @@ import datetime as _dt
 import pydantic as _pydantic
 
 
-class _PostBase(_pydantic.BaseModel):
-    """Base schema for 'Post' data, including title and content."""
-
-    title: str
-    content: str
-
-
-class CreatePost(_PostBase):
-    """Schema for creating a new Post.
-    Requires title and content, used for data validation and creation.
-    """
-
-
-class Post(_PostBase):
-    """Schema for representing a Post including additional details.
-    Inherits from '_PostBase' and adds fields like ID, owner ID,
-    creation and update dates.
-    """
-
-    id: int
-    owner_id: int
-    date_created: _dt.datetime
-    date_last_updated: _dt.datetime
-
-    class Config:
-        orm_mode = True
-
-
 class _UserBase(_pydantic.BaseModel):
     """Base schema for User data, including email."""
-
     email: str
 
 
@@ -45,7 +16,6 @@ class UserCreate(_UserBase):
     """Schema for creating a new User.
     Requires email and password, used for data validation and creation.
     """
-
     password: str
 
 
@@ -53,10 +23,10 @@ class User(_UserBase):
     """Schema for representing a User including additional details and posts.
     Inherits from _UserBase and adds fields like ID, active status, and a list of posts.
     """
+    preferences:str
 
     id: int
     is_active: bool
-    posts: list[Post] = []
 
     class Config:
         orm_mode = True
@@ -64,7 +34,6 @@ class User(_UserBase):
 
 class RecommendedPaper(_pydantic.BaseModel):
     """Base schema for RecommendedPaper data, including basic paper details."""
-
     id: str
     title: str
     published_date: _dt.datetime
